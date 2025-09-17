a45a5097f716e7f6f55b409e15730dadd699c2f0
Mahdi <mahd8767trfx@gmail.com>
2025-09-17 17:44:17 +0330

    install project

---
+++ src/app/server/users.ts
@@ -0,0 +1,9 @@
+import { User } from "../types/IUser";
+
+export const Users:User[]=[
+    {id:1,name:'Json',password:'G7v!pL2#',role:'admin'},
+    {id:2,name:'Mahdi',password:'mN$4kR!q',role:'admin'},
+    {id:3,name:'Mary',password:'B8x@tZ6w',role:'user'},
+    {id:4,name:'Jack',password:'pQ#3sV!y',role:'user'},
+    {id:5,name:'Tina',password:'L!9bXr$2',role:'user'}
+]
